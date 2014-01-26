e514302094c1d2152c97b1c800d1cf8d1dbdd865
Simonas <simonasgildutis@gmail.com>
2014-01-26 18:39:51 +0200

    Finished excercises

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,4 +1,6 @@
 class MicropostsController < ApplicationController
+	include MicropostsHelper
+
 	before_action :signed_in_user, only: [:create, :destroy]
 	before_action :correct_user, only: :destroy
 
